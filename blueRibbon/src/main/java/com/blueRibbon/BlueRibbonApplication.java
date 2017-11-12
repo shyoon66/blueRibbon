66c16844109bbeaeccd946aaeb5d0357d5af7d5b
Cynthia_A <Cynthia_A@DESKTOP-0BFEEKT>
2017-11-12 17:07:27 +0900

    first commit

---
+++ blueRibbon/src/main/java/com/blueRibbon/BlueRibbonApplication.java
@@ -0,0 +1,12 @@
+package com.blueRibbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueRibbonApplication.class, args);
+	}
+}
---
+++ blueRibbon/src/test/java/com/blueRibbon/BlueRibbonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blueRibbon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlueRibbonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
