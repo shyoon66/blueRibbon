179d45868ea5e85c8335dbce75bf9ec35ee47ab6
Cynthia_A <Cynthia_A@DESKTOP-0BFEEKT>
2017-11-12 18:38:35 +0900

    commit

---
+++ blueRibbon/src/main/java/com/blueRibbon/home/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.blueRibbon.home.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "helloWorld!";
+	}
+}
