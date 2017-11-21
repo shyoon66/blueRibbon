8b7be7da31a8352f6153b71176fb9d06b74a3bb8
Cynthia_A <Cynthia_A@DESKTOP-0BFEEKT>
2017-11-22 01:04:59 +0900

    fegsdfsdf

--- blueRibbon/src/main/java/com/blueRibbon/notice/controller/NoticeController.java
+++ blueRibbon/src/main/java/com/blueRibbon/notice/controller/NoticeController.java
@@ -3,11 +3,16 @@ package com.blueRibbon.notice.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.blueRibbon.notice.dao.NoticeDao;
+import com.blueRibbon.notice.model.Notice;
 
 @Controller
 @RequestMapping("/notice")
@@ -17,16 +22,11 @@ public class NoticeController {
 	
 	@Autowired
 	private NoticeDao noticeDao;
-
+	
 	@RequestMapping("/")
-	public String notice(Model model) throws Exception {
-		model.addAttribute("list", noticeDao.findAll());
+	public String notice(Model model, @PageableDefault(sort = {"create_dt"}, direction = Direction.DESC, size = 20) Pageable pageable) throws Exception {
+		Page<Notice> postPage = noticeDao.findAll(pageable);
+		model.addAttribute("list", postPage);
 		return "/notice/notice";
 	}
-	
-/*	@RequestMapping("/list")
-	public List<Notice> list(Model model) throws Exception {
-		List<Notice> noticeList = noticeDao.findAll();
-		return noticeList;
-	}*/
 }
--- blueRibbon/src/main/webapp/WEB-INF/jsp/home.jsp
+++ blueRibbon/src/main/webapp/WEB-INF/jsp/home.jsp
@@ -202,7 +202,6 @@
       <div class="container">
         <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
       </div>
-      <!-- /.container -->
     </footer>
 
     <!-- Bootstrap core JavaScript -->
